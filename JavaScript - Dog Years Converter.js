a38c9a069a84d6b761d93f182eb50daf93dd2226
Francesco <f.foresta86@gmail.com>
2022-01-08 17:38:21 +0100

    Create JavaScript - Dog Years Converter.js

---
+++ JavaScript - Dog Years Converter.js
@@ -0,0 +1,16 @@
+// My age
+const myAge = 35;
+// A dog's early years
+let earlyYears = 2;
+// Converting early years to dog years
+earlyYears *= 10.5;
+// Subtracting the early years
+let laterYears = myAge - 2;
+// Calculating the number of dog years accounted for by my later years
+laterYears *= 4;
+// Calculating my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+// My name
+const myName = "Francesco";
+// Printing out
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
