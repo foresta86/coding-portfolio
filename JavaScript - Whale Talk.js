53284dc0d08e5e7baf7ba725072fdc64bd703c3f
Francesco <f.foresta86@gmail.com>
2022-01-24 10:15:20 +0100

    Create JavaScript - Whale Talk.js

---
+++ JavaScript - Whale Talk.js
@@ -0,0 +1,18 @@
+const input = "Hey there, my name is Francesco.";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  // console.log("inputIndex is " + input[inputIndex]);
+  for (let vowel = 0; vowel < vowels.length; vowel++) {
+    if (input[inputIndex] === vowels[vowel]) {
+      if (input[inputIndex] === "e") {
+        resultArray.push("ee");
+      } else if (input[inputIndex] === "u") {
+        resultArray.push("uu");
+      } else {
+        resultArray.push(input[inputIndex]);
+      }
+    }
+  }
+}
+console.log(resultArray.join("").toUpperCase());
